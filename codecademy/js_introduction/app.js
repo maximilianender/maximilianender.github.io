e611da23423ac2d248b7e43d755b9686984b88b3
maximilianender <@maximilian.w.ender@student.uibk.ac.at>
2021-03-19 14:49:31 +0100

    methods

--- codecademy/js_introduction/app.js
+++ codecademy/js_introduction/app.js
@@ -21,3 +21,8 @@ console.log(0.2708*100)
 console.log("Hello" + "World")
 console.log("Hello" + " " + "World")
 console.log("Teaching the world how to code".length)
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
