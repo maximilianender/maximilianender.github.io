573b065ba401a14b4f819cce97cdeb91d7f5dab7
maximilianender <@maximilian.w.ender@student.uibk.ac.at>
2021-03-25 16:20:06 +0100

    marker

--- nz/main.js
+++ nz/main.js
@@ -3,10 +3,11 @@ console.log(L);
 
 const map = L.map("map", {
     center: [-41.293056, 174.001944],
-    zoom: 13,
+    zoom: 14,
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
 });
 
+let mrk = L.marker([-41.293056, 174.001944]).addTo(map);
 console.log(document.querySelector("#map"))
