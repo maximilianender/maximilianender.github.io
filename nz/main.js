8d89e89d3cb5580a6869c313fb08cd3dcbc5d14c
maximilianender <@maximilian.w.ender@student.uibk.ac.at>
2021-03-25 17:04:30 +0100

    popup verändert

--- nz/main.js
+++ nz/main.js
@@ -21,8 +21,8 @@ const map = L.map("map", {
 });
 
 let mrk = L.marker([stop.lat, stop.lng]).addTo(map);
-    mrk.bindPopup(`<h4> ${stop.nr}: ${stop.name}</h4>Stop
-    <p><i class="fasfa-external-link-alt mr-3><i><a href="${stop.wikipdia}">Read about stop in Wikipedia</a></p>`).openPopup();
+    mrk.bindPopup(`<h4> ${stop.nr}: ${stop.name}</h4>
+    <p><i class="fasfa-external-link-alt mr-3><i><a href="${stop.wikipedia}">Read about stop in Wikipedia</a></p>`).openPopup();
 
 
 //console.log(document.querySelector("#map"))
