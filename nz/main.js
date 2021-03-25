fcaf0303df272bb41b7b9418e6adf2be7aec1eb6
maximilianender <@maximilian.w.ender@student.uibk.ac.at>
2021-03-25 16:16:59 +0100

    karte erstellt

--- nz/main.js
+++ nz/main.js
@@ -1,5 +1,12 @@
 console.log("Hello World!");
 console.log(L);
 
-const map = L.map("map");
+const map = L.map("map", {
+    center: [-41.293056, 174.001944],
+    zoom: 13,
+    layers: [
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+    ]
+});
+
 console.log(document.querySelector("#map"))
