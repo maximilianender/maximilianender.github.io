3e83726a60ca7408dcf6e0a13923bd174b4a08c4
maximilianender <@maximilian.w.ender@student.uibk.ac.at>
2021-03-25 17:39:58 +0100

    kleine veränderungen

--- nz/main.js
+++ nz/main.js
@@ -13,16 +13,16 @@ let stop = {
 
 
 const map = L.map("map", {
-    center: [stop.lat, stop.lng],
-    zoom: 10,
+    //center: [stop.lat, stop.lng],
+    //zoom: 10,
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
 });
 
-console.log(ROUTE);
+//console.log(ROUTE);
 for (let entry of ROUTE) {
-    console.log(entry);
+    //console.log(entry);
 
     let mrk = L.marker([entry.lat, entry.lng]).addTo(map);
     mrk.bindPopup(`<h4> ${entry.nr}: ${entry.name}</h4><p><i class="fas fa-external-link-alt mr-3"></i><a href="${entry.wikipedia}">Read about stop in Wikipedia</a></p>`);
