89239b4f5eec58c241b49518c7bc0dd1504a53d2
maximilianender <@maximilian.w.ender@student.uibk.ac.at>
2021-03-25 14:34:16 +0100

    beautify file

--- codecademy/js_introduction/app.js
+++ codecademy/js_introduction/app.js
@@ -14,10 +14,10 @@ console.log("JavaScript")
 console.log(2011)
 console.log("Woohoo! I love to code! #codecademy")
 console.log(20.49)
-console.log(22+3.5)
-console.log(2021-1969)
-console.log(65/240)
-console.log(0.2708*100)
+console.log(22 + 3.5)
+console.log(2021 - 1969)
+console.log(65 / 240)
+console.log(0.2708 * 100)
 console.log("Hello" + "World")
 console.log("Hello" + " " + "World")
 console.log("Teaching the world how to code".length)
@@ -26,7 +26,7 @@ console.log('Codecademy'.toUpperCase());
 
 // Use a string method to log the following string without whitespace at the beginning and end of it.
 console.log('    Remove whitespace   '.trim());
-console.log(Math.random()*100);
-console.log(Math.floor(Math.random()*100))
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 100))
 console.log(Math.ceil(43.8))
 console.log(Number.isInteger(2017))
--- codecademy/js_variables/main.js
+++ codecademy/js_variables/main.js
@@ -1,9 +1,9 @@
-var favoriteFood = "pizza" ;
+var favoriteFood = "pizza";
 console.log(favoriteFood);
-var numOfSlices = 8 ;
+var numOfSlices = 8;
 console.log(numOfSlices)
 let changeMe = true;
-changeMe = false; 
+changeMe = false;
 console.log(changeMe)
 const entree = "Enchiladas";
 console.log(entree);
@@ -25,9 +25,9 @@ quarterMe /= 4;
 
 // These console.log() statements below will help you check the values of the variables.
 // You do not need to edit these statements. 
-console.log('The value of levelUp:', levelUp); 
-console.log('The value of powerLevel:', powerLevel); 
-console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of levelUp:', levelUp);
+console.log('The value of powerLevel:', powerLevel);
+console.log('The value of multiplyMe:', multiplyMe);
 console.log('The value of quarterMe:', quarterMe);
 let gainedDollar = 3;
 let lostDollar = 50;
@@ -37,7 +37,7 @@ console.log(gainedDollar);
 
 lostDollar--;
 console.log(lostDollar);
-let favoriteAnimal = "Tasmanian Devil"; 
+let favoriteAnimal = "Tasmanian Devil";
 console.log("My favorite animal: " + favoriteAnimal);
 let myName = "Maximilian";
 let myCity = "Innsbruck";
@@ -47,6 +47,3 @@ console.log(typeof newVariable)
 
 newVariable = 1;
 console.log(typeof newVariable)
-
-
-
