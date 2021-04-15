0b9530b079a34e0f4b97ea73d517ea89fcdb7e8b
maximilianender <@maximilian.w.ender@student.uibk.ac.at>
2021-04-15 17:15:37 +0200

    add date and temp

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -35,7 +35,13 @@ for(station of json.features) {
     let marker = L.marker([
     station.geometry.coordinates[1],
     station.geometry.coordinates[0]]);
-    marker.bindPopup(`<h3>${station.properties.name}</h3>`);
+    marker.bindPopup(`
+    <h3>${station.properties.name}</h3>
+    <ul>
+        <li>Datum: ${station.properties.date}</li>
+        <li>Temperatur: ${station.properties.LT}</li>
+    </ul>
+    `);
     marker.addTo(awsLayer);
 }
 // set map view to all stations
