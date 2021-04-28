0247461cab7569a2e435b796462e2472b8faf6e4
maximilianender <@maximilian.w.ender@student.uibk.ac.at>
2021-04-28 11:01:16 +0200

    kleine veränderungen

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -122,12 +122,12 @@ fetch(awsUrl).then(response => response.json())
     <h3>${station.properties.name}</h3>
     <ul>
         <li>Datum: ${formattedDate.toLocaleString("de")} Uhr</li>
-        <li>Seehöhe: ${station.geometry.coordinates[2] ||"?"} m.ü.A.</li>
-        <li>Temperatur: ${station.properties.LT||"?"} °C</li>
-        <li>Relative Luftfeuchtigkeit: ${station.properties.RH||"?"} %</li>
-        <li>Schneehöhe: ${station.properties.HS||"?"} cm</li>
-        <li>Windgeschwindigkeit: ${station.properties.WG||"?"} km/h</li>
-        <li>Windrichtung: ${getDirection(station.properties.WR, DIRECTIONS)|| "?"} </li>
+        <li>Seehöhe: ${station.geometry.coordinates[2] ||"-"} m.ü.A.</li>
+        <li>Temperatur: ${station.properties.LT||"-"} °C</li>
+        <li>Relative Luftfeuchtigkeit: ${station.properties.RH||"-"} %</li>
+        <li>Schneehöhe: ${station.properties.HS||"-"} cm</li>
+        <li>Windgeschwindigkeit: ${station.properties.WG||"-"} km/h</li>
+        <li>Windrichtung: ${getDirection(station.properties.WR, DIRECTIONS)|| "-"} </li>
     </ul>
     <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
     `);
