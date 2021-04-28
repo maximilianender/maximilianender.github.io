09e80691628cb21e5590488e9bdd7c6f05190efb
maximilianender <@maximilian.w.ender@student.uibk.ac.at>
2021-04-28 10:58:07 +0200

    kleine veränderungen

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -174,7 +174,7 @@ fetch(awsUrl).then(response => response.json())
                 let marker = WindLabel(station.geometry.coordinates, {
                     value: station.properties.WR,
                     directions: DIRECTIONS,
-                    colors: DIRECTIONS.col,
+                    colors: COLORS.winddirection,
                     station: station.properties.name
                 });
                 marker.addTo(overlays.winddirection);
