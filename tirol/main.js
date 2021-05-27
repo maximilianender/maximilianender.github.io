cabc043ad0116657416b734ccc799372b23397df
maximilianender <@maximilian.w.ender@student.uibk.ac.at>
2021-05-27 16:27:19 +0200

    article mit icons nach typ anzeigen

--- tirol/main.js
+++ tirol/main.js
@@ -70,7 +70,19 @@ const drawWikipedia = (bounds) => {
         console.log(jsonData);
         // Artikel Marker erzeugen
         for(let article of jsonData.geonames) {
-            let mrk = L.marker([article.lat, article.lng]);
+            // Welches Icon soll verwendet werden? 
+            if(icons[article.feature]) {
+                // ein Bekanntes
+            } else {
+                // Generisches Info-Icon
+                article.feature = "default";
+            };
+
+            let mrk = L.marker([article.lat, article.lng], {
+                icon: L.icon({
+                    iconUrl: `icons/${icons[article.feature]}`
+                })
+            });
             mrk.addTo(overlays.wikipedia);
 
             // optionales Bild definieren
