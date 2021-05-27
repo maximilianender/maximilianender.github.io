db34a90f100532aee2e661394f7a8de7140338a2
maximilianender <@maximilian.w.ender@student.uibk.ac.at>
2021-05-27 17:43:36 +0200

    weblink als neuen Baustein definiert

--- tirol/main.js
+++ tirol/main.js
@@ -167,7 +167,8 @@ const updateTexts = (nr) => {
         //console.log(etappe);
         // Ist es die aktuelle Etappe? 
         if (etappe.nr == nr) {
-            console.log("unsere Etappe", etappe);
+            //console.log("unsere Etappe", etappe);
+            etappe.homepage = `<a href="${etappe.weblink}">Homepage</a>`
             for (let key in etappe) {
                 console.log("key:", key, "value", etappe[key]);
                 // gibt es ein Element im HTML mit der ID von "key"
